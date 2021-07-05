e642b139c938cc396570c49e143aec2738412213
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-07-05 22:11:56 +0300

    Create server

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+const express = require('express');   //   import  server  requirments 
+
+require('dotenv').config();  //   to  activate  .env  file 
+
+
+const cors = require('cors');
+
+const wheather = require('./data/wheather.json'); //  Local  Data  in JSON file
+
+
+const server = express();  //   name  of  the  server,  to manage process
+const PORT = process.env.PORT;  //  PORT  name  and  number 
+server.use(cors());
+
+
+//  Requests  &  Responses  //
+//  wheather => link //  ?city=cityName => request  // 
+server.get('/wheather', (req, res) => {
+    let city = req.query.city.toLocaleLowerCase();
+    
+    // console.log(wheather[0])
+    // console.log(req.query.city.toLocaleLowerCase())
+    let cityInfo = wheather.filter((item)=>{
+        if(city === item.city_name.toLocaleLowerCase()){
+            return true
+        }
+    })
+    res.send(cityInfo)
+})
+//  Requests  &  Responses  //
+
+
+server.listen(PORT, () => {  //   function  to  awake  Server 
+    console.log(`Listening on PORT ${PORT}`);
+})
